f22fbb3f926130de910ae832d257c760e8ba8003
Shaun Jacobsen <shaun.d.jacobsen@gmail.com>
2017-01-18 15:22:45 -0600

    some work

--- page-validator/application.rb
+++ page-validator/application.rb
@@ -10,46 +10,51 @@ require "open-uri"
 require "cgi"
 require "capybara"
 require "capybara/poltergeist"
+require "capybara/dsl"
+require "uri"
 
 # configuration
 
+include Capybara::DSL
+Capybara.default_driver = :poltergeist
+
+Capybara.register_driver :poltergeist do |app|
+  Capybara::Poltergeist::Driver.new(app, {js_errors: false})
+end
+
+UI_JS_VALUES = [
+  URI("http://ui.powerreviews.com/stable/4.0/ui.js"),
+  URI("https://ui.powerreviews.com/stable/4.0/ui.js")
+  ]
+
 configure do
   enable :sessions
+  enable :logging
   set :session_secret, 'secret'
   set :erb, :escape_html => true
 end
 
 before do
-  @files = Dir.glob("data/*").map do |path|
-    File.basename(path)
+  @loaded_elements ||= {}
+  @pr_elements ||= []
+end
+
+def get_network_traffic(page)
+  visit(page)
+  page.driver.network_traffic.each do |request|
+    request.response_parts.each do |response|
+      @loaded_elements[response.url] = response.status
+    end
   end
 end
 
-def data_path
-  if ENV["RACK_ENV"] == "test"
-    File.expand_path("../tests/data", __FILE__)
-  else
-    File.expand_path("../data", __FILE__)
-  end
-end
-
-def load_user_list
-  user_list = YAML.load(File.open('users.yml'))
-  users_array = []
-  user_list.each do |k, v|
-    users_array << [k, BCrypt::Password.new(v)]
-  end
-  users_array
-end
-
-def signed_in?
-  session[:user].nil?
-end
-
-def redirect_to_login?
-  if signed_in?
-    session[:error] = "You must be signed in to do that."
-    redirect "/"
+def get_powerreviews_files
+  @loaded_elements.each do |k, v|
+    url = URI(k)
+    #puts url.path.to_s.split('/').last
+    if url.host =~ /powerreviews.com/i
+      @pr_elements << url
+    end
   end
 end
 
@@ -64,10 +69,6 @@ def check_protocol(url)
   end
 end
 
-def fulljs_present?
-  @page.css("full.js").first
-end
-
 def get_meta_attributes(page)
   meta = {}
   meta[:title] = page.css('head>title').text || page.css('head>meta[@name=title')
@@ -76,11 +77,34 @@ def get_meta_attributes(page)
   return meta
 end
 
-helpers do
-  def paragraphify(text)
-    text.gsub("\n", "<br />")
-  end
+def get_filename(url)
+end
 
+def ui_js_present?(element_list = @pr_elements)
+  element_list.any? { |resource| UI_JS_VALUES.include? resource }
+  return element_list
+end
+
+def full_js_present?(element_list = @pr_elements)
+  file_location = ""
+  element_list.each do |resource|
+    if (resource.host == "cdn.powerreviews.com" && resource.path.to_s.split('/').last == "full.js")
+      file_location = "CDN"
+    end
+  end
+  file_location == "CDN"
+end
+
+def external_full_js_present?(element_list)
+
+end
+
+def all_conditions_listing
+  ui_js_present?(@pr_elements) ? "ui.js present" : "ui.js not present"
+  full_js_present?(@pr_elements) ? "full.js present" : "full.js not present"
+end
+
+helpers do
   def current_working_url
     if session[:url]
       session[:url]
@@ -96,21 +120,10 @@ helpers do
       return "fa-exclamation-circle"
     end
   end
+
   def render_markdown(text)
     markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML)
     markdown.render(text)
-  end
-
-  def valid_filetype?(filename)
-    %w(txt md).include? filename.split(".").last
-  end
-
-  def filename_taken?(filename)
-    @files.include? filename
-  end
-
-  def valid_filename?(filename)
-    filename.strip.length > 0 && !filename_taken?(filename) && valid_filetype?(filename)
   end
 end
 
@@ -123,6 +136,9 @@ end
 post "/get" do
   session[:url] = params[:url]
   check_protocol(params[:url])
+  get_network_traffic(params[:url])
+  get_powerreviews_files
+
   redirect "/result"
 end
 
@@ -130,99 +146,8 @@ get "/result" do
   @url = session[:url]
   @page = Nokogiri::HTML(open(@url))
   @meta = get_meta_attributes(@page)
+
   erb :result, layout: :layout
 end
 
-get "/read/*.*" do |path, ext|
-  filename = path
-  filetype = ext
-  filepath = File.join(data_path,"#{filename}.#{filetype}")
-  if File.exist?(filepath)
-    case filetype
-    when 'txt'
-      content_type :text
-      File.read(filepath)
-    when 'md'
-      render_markdown(File.read(filepath))
-    end
-  else
-    session[:error] = "#{filename}.#{filetype} does not exist."
-    redirect "/"
-  end
-end
-
-get "/edit/:file" do
-  redirect_to_login?
-  @filename = params[:file]
-  filepath = File.join(data_path,@filename)
-  @contents = File.read(filepath)
-  erb :edit, layout: :layout
-end
-
-post "/edit/:file" do
-  redirect_to_login?
-  filename = params[:file]
-  filepath = File.join(data_path,filename)
-  new_contents = params[:contents]
-  rewrite_file = File.open(filepath, "w") do |file|
-    file.write new_contents
-  end
-  if rewrite_file
-    session[:messages] = "#{filename} updated."
-    redirect "/"
-  else
-    session[:error] = "There was an error updating #{filename}."
-  end
-end
-
-get "/new" do
-  redirect_to_login?
-  erb :new, layout: :layout
-end
-
-post "/new" do
-  redirect_to_login?
-  filename = params[:filename]
-  if valid_filename?(filename)
-    f = File.new("data/#{filename}", "w")
-    redirect "/"
-  else
-    session[:error] = "#{filename} is not a valid filename."
-    redirect "/new"
-  end
-end
-
-post "/delete/:filename" do
-  redirect_to_login?
-  filename = params[:filename]
-  filepath = File.join(data_path,filename)
-  File.delete(filepath)
-  session[:messages] = "#{filename} has been deleted."
-  redirect "/"
-end
-
-get "/login" do
-  @username = session[:attempted_username]
-  erb :login, layout: :layout
-end
-
-post "/login" do
-  credentials = [params[:username], params[:password]]
-  if load_user_list.include? credentials
-    session[:user] = params[:username]
-    session[:messages] = "Welcome!"
-    redirect "/"
-  else
-    session[:error] = "Invalid Credentials"
-    session[:attempted_username] = params[:username]
-    redirect "/login"
-  end
-end
-
-get "/logout" do
-  session.delete(:user)
-  session.delete(:attempted_username)
-  session[:messages] = "You have been logged out."
-  redirect "/"
-end
 
--- page-validator/scrape.rb
+++ page-validator/scrape.rb
@@ -11,18 +11,65 @@ Capybara.register_driver :poltergeist do |app|
   Capybara::Poltergeist::Driver.new(app, {js_errors: false})
 end
 
-loaded_elements = {}
+UI_JS_VALUES = [
+  URI("http://ui.powerreviews.com/stable/4.0/ui.js"),
+  URI("https://ui.powerreviews.com/stable/4.0/ui.js")
+  ]
+
+FULL_JS_VALUES = [
+  
+]
+
+@loaded_elements = {}
+@pr_elements = []
 
 visit "http://www.sjacobsen.com/pr/cd4/index.html"
 
 page.driver.network_traffic.each do |request|
-  request.response_parts.uniq(&:url).each do |response|
-    loaded_elements[response.url] = response.status
+  request.response_parts.each do |response|
+    @loaded_elements[response.url] = response.status
     #puts "#{response.url}: #{response.status}"
   end
 end
 
-loaded_elements.each do |k, v|
+@loaded_elements.each do |k, v|
   url = URI(k)
-  puts url.path if url.host == "ui.powerreviews.com"
+  #puts url.path.to_s.split('/').last
+  if url.host =~ /powerreviews.com/i
+    @pr_elements << url
+  end
 end
+
+@pr_elements.each do |element|
+  if element.path.split('/').last == "v1.gif"
+    element.query.split('&')
+  end
+end
+
+def get_filename(url)
+end
+
+def ui_js_present?(element_list)
+  element_list.any? { |resource| UI_JS_VALUES.include? resource }
+end
+
+def full_js_present?(element_list)
+  file_location = ""
+  element_list.each do |resource|
+    if (resource.host == "cdn.powerreviews.com" && resource.path.to_s.split('/').last == "full.js")
+      file_location = "CDN"
+    end
+  end
+  file_location == "CDN"
+end
+
+def external_full_js_present?(element_list)
+
+end
+
+def all_conditions_listing
+  ui_js_present?(@pr_elements) ? "ui.js present" : "ui.js not present"
+  full_js_present?(@pr_elements) ? "full.js present" : "full.js not present"
+end
+
+puts all_conditions_listing
